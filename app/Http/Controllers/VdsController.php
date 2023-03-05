0afca479719fa5980680c028126a1b3bc20455c3
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2023-03-05 22:08:11 +0300

    fdsaf

--- app/Http/Controllers/VdsController.php
+++ app/Http/Controllers/VdsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Vd;
 use Illuminate\Http\Request;
 
 class VdsController extends Controller
@@ -10,5 +11,15 @@ class VdsController extends Controller
     {
         return response('Здесь будет перечень видео роликов')
             ->header('Content-Type', 'text/plain');
+    }
+
+    public function detail(Vd $vd)
+    {
+        $s = $vd->title . "\r\n\r\n";
+        $s .= $vd->content . "\r\n";
+        $s .= $vd->price . "\r\n";
+        $s .= $vd->ball . "\r\n";
+        $s .= $vd->place . "\r\n";
+        return response($s)->header('Content-Type', 'text/plain');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use App\Http\Controllers\VdsController;
 */
 
 Route::get('/', [VdsController::class, 'index']);
+Route::get('/{vd}',[VdsController::class,'detail']);
