f6f4bf43ad3dbaf150f6c7f341d18eddc4fa1fdf
Dmitriy <dmitriy.folomkin@gmail.com>
2023-03-06 14:37:44 +0300

    Вывод спортсменов на Главной, Странца Детализация

--- app/Http/Controllers/VdsController.php
+++ app/Http/Controllers/VdsController.php
@@ -9,17 +9,12 @@ class VdsController extends Controller
 {
     public function index()
     {
-        return response('Здесь будет перечень видео роликов')
-            ->header('Content-Type', 'text/plain');
+        $context = ['vds' => Vd::latest()->get()];
+        return view('index', $context);
     }
 
     public function detail(Vd $vd)
     {
-        $s = $vd->title . "\r\n\r\n";
-        $s .= $vd->content . "\r\n";
-        $s .= $vd->price . "\r\n";
-        $s .= $vd->ball . "\r\n";
-        $s .= $vd->place . "\r\n";
-        return response($s)->header('Content-Type', 'text/plain');
+        return view('detail', ['vd'=>$vd]);
     }
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Объявления</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+            integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+            integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+            crossorigin="anonymous"></script>
+</head>
+</head>
+<body>
+<div class="container">
+    <h1 class="my-3 text-center">Видеоролики</h1>
+    @if (count($vds) > 0)
+
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Участник</th>
+                <th>Видео</th>
+{{--                <th>Баллы</th>--}}
+{{--                <th>Место</th>--}}
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($vds as $vd)
+                <tr>
+                    <td><h3>{{ $vd->title }}</h3></td>
+                    <td>
+                        <p>{{ $vd->content }}</p>
+                        <p><a href="/{{ $vd->id }}/">Подробнее...</a></p>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @endif
+</div>
+</body>
+</html>
